325f5ddb3e3d33d3a470793709e4848c2a6f9585
Abhishek <neupaneabhishek13@gmail.com>
2023-06-12 20:30:37 +0545

    final push of multifunctional clock

--- Simple minor codes/multi functional clock/script.js
+++ Simple minor codes/multi functional clock/script.js
@@ -40,6 +40,9 @@ function setTimer(duration, displayElement) {
   
     const intervalId = setInterval(function () {
       minutes = Math.floor(timer / 60);
+
+      //Math.floor rounds down the given number(eg: 1.2 is rounded down to 1)
+
       seconds = timer % 60;
   
       minutes = minutes < 10 ? "0" + minutes : minutes;
@@ -57,15 +60,73 @@ function setTimer(duration, displayElement) {
 
 
   //For stopwatch
-
-  document.getElementById('start').addEventListener("click", startStopWatch());
-
-  function startStopWatch(x){
-    
-    document.getElementById('count').innerHTML=x;
-    x++;
+  
+  document.getElementById('start').addEventListener("click", () => startStopWatch());
+  document.getElementById('pause').addEventListener('click', () => setFlag(event));
+  document.getElementById('lap').addEventListener('click', () => setFlag(event));
+  document.getElementById('stop').addEventListener('click', () => setFlag(event));
+  document.getElementById('play').addEventListener('click', () => setFlag(event));
+  
+  var x = 0;
+  var flag = 0;
+  var intervalId;
+  var laps =[];
+  
+  function setFlag(event) {
+    var etarget = event.target.id;
+    if (etarget == 'pause') {
+      flag = 1;
+    } else if (etarget == 'lap') {
+      flag = 2;
+    } else if (etarget == 'stop') {
+      flag = 3;
+    } else if (etarget == 'play') {
+      flag = 0;
+      startStopWatch();
+    }
   }
+  
+  function startStopWatch() {
+    flag=0;
+    var show, minutes, seconds, milli,lapout,i=1;
+    intervalId = setInterval(function () {
+      if (flag !== 1) {  //if flag is 1, the logic is stopped hence pausing the stopwatch
+        minutes = Math.floor(x / 6000);
+        seconds = Math.floor(x / 100);
+        if (seconds >= 60) {
+          seconds = seconds % 60;
+        }
+        milli = x % 100;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        show = minutes + " : " + seconds + " : " + milli;
+        document.getElementById('count').innerHTML = show;
+        if (flag == 2) { //if flag is 2,lap is shown.
+          document.getElementById('lapdisp').innerHTML = "Lap " + show;
+          //pushing the laps to display later;
+          laps.push(show);
+          flag=0;
+        }
+        if (flag == 3) {   //flag 3 is for stopping the stopwatch
+          clearInterval(intervalId);
+          document.getElementById('count').innerHTML = "00:00:00";
+          
+          //Displaying the laps after stopping
+          laps.forEach(function(element) {
+            lapout=lapout+"\nLAP"+i+": "+element;
+            i++;
+          });
+          document.getElementById('lapdisp').innerHTML = lapout;
+        }
+        x++;
+        
+      }
+    }, 10);
+    laps.length=0;//Clearing the laps
+  }
+  
 
-  const intervalId=setInterval(startStopWatch,1000);
+  
+  
   
 
